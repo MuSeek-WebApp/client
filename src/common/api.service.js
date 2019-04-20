91150a3ed2ecee7b3c8340354ebf52cd37c8ce21
Yossig <yosigartner@gmail.com>
2019-04-20 18:20:48 +0300

    edit base URL to match server configurations

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import { API_URL } from "@/common/config";
+import BASE_URL from "@/common/config";
 
 const ApiService = {
   init() {
     Vue.use(VueAxios, axios);
-    Vue.axios.defaults.baseURL = API_URL;
+    Vue.axios.defaults.baseURL = BASE_URL;
   },
 
   get(resource) {
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   devServer: {
     proxy: {
+      "/auth": {
+        target: "http://localhost:3000",
+        changeOrigin: true
+      },
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true
