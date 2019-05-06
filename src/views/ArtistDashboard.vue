81927d5e4987408e416c3a61fee493e403fda994
Yossi Gartner <yosigartner@gmail.com>
2019-05-06 17:46:47 +0300

    add event sorting by date and different sections by status (#20)

--- src/views/ArtistDashboard.vue
+++ src/views/ArtistDashboard.vue
@@ -1,25 +1,29 @@
 <template>
-  <v-container grid-list-md>
+  <v-container grid-list-xl>
     <v-layout>
       <v-flex md8>
         <v-card height="500" class="scroll-y">
           <v-card-title>
-            <h3 class="pa-3">Plan Your Next Gig!</h3>
+            <h1 class="pa-3">Plan Your Next Gig!</h1>
           </v-card-title>
           <v-card-text></v-card-text>
         </v-card>
       </v-flex>
       <v-flex md4>
-        <v-sheet class="elevation-2" tile>
-          <h3 class="pa-3">Upcoming Events</h3>
-        </v-sheet>
-        <v-card max-height="500" class="scroll-y">
+        <v-card max-height="800" class="scroll-y">
           <v-card-text>
-            <SmallViewEvent
-              :event="{ ...event }"
-              :key="event._id"
-              v-for="event in events"
-            ></SmallViewEvent>
+            <h1 class="mb-1">Upcoming Events</h1>
+            <div :key="status" v-for="status in Object.keys(displayStatuses)">
+              <template v-if="mapEvents[status]">
+                <v-divider class="mb-3"></v-divider>
+                <h3 class="mb-3">{{ displayStatuses[status] }}</h3>
+                <SmallViewEvent
+                  :event="{ ...event }"
+                  :key="event._id"
+                  v-for="event in mapEvents[status]"
+                ></SmallViewEvent>
+              </template>
+            </div>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -30,18 +34,43 @@
 <script>
 import SmallViewEvent from "../components/SmallViewEvent.vue";
 import { FETCH_EVENTS } from "../store/actions.type.js";
-
+import moment from "moment";
 export default {
   components: {
     SmallViewEvent
   },
   data: function() {
     return {
-      events: this.$store.getters.getAllEvents
+      events: this.$store.getters.getAllEvents,
+      displayStatuses: {
+        APPROVED: "Approved Events",
+        WAITING_FOR_BAND_APPROVAL: "Waiting for your approval",
+        WAITING_FOR_BUSINESS_APPROVAL: "Waiting for business approval",
+        DENIED: "Ignored Events"
+      }
     };
   },
   created() {
     this.$store.dispatch(FETCH_EVENTS);
+  },
+  computed: {
+    mapEvents() {
+      const map = {};
+      this.events.forEach(e =>
+        (map[e.requests.status] = map[e.requests.status] || []).push(e)
+      );
+
+      for (let status in map) {
+        map[status].sort((e1, e2) => {
+          return (
+            moment(e1.startDate + " " + e1.startTime) -
+            moment(e2.startDate + " " + e2.startTime)
+          );
+        });
+      }
+
+      return map;
+    }
   }
 };
 </script>
