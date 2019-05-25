5ac1ab8dc3bb19935be754435f5164b2427080b9
Yossig <yosigartner@gmail.com>
2019-05-25 20:09:04 +0300

    change custom rating to work with like\dislike

--- src/components/CustomRating.vue
+++ src/components/CustomRating.vue
@@ -1,33 +1,28 @@
 <template>
-  <v-container class="pa-0">
-    <v-layout>
-      ({{ rating || 0 }})
-
-      <v-rating
-        :value="rating || 0"
-        dense
-        readonly
-        background-color="orange lighten-3"
-        color="orange"
-        small
-      ></v-rating>
-    </v-layout>
-  </v-container>
+  <v-layout class="font-weight-bold">
+    <v-icon>thumb_up_off_alt</v-icon>
+    {{ likes }}
+    <v-icon class="ml-3">thumb_down_off_alt</v-icon>
+    {{ dislikes }}
+  </v-layout>
 </template>
 
 <script>
 import ApiService from "../common/api.service";
 export default {
-  props: ["passedRating", "userId"],
+  props: ["userId"],
   data: function() {
     return {
-      rating: this.passedRating
+      rating: this.passedRating,
+      likes: 0,
+      dislike: 0
     };
   },
   async created() {
     if (this.userId) {
       const { data } = await ApiService.get("api/user/rating/" + this.userId);
-      this.rating = Number(data[0].avgRating.toFixed(1));
+      this.likes = data[0].likes;
+      this.dislikes = data[0].dislikes;
     }
   }
 };
