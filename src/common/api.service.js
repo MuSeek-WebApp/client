d2f138dfc576eaf97339ff3eab61faf7b2e949ce
Yossig <yosigartner@gmail.com>
2019-06-07 16:13:48 +0300

    remove use of config.js, use .env instead

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import BASE_URL from "@/common/config";
 
 const ApiService = {
   init() {
     Vue.use(VueAxios, axios);
-    Vue.axios.defaults.baseURL = BASE_URL;
+    Vue.axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
   },
   get(resource, params) {
     return Vue.axios.get(`${resource}`, { params });
--- src/common/config.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = "http://localhost:8080";
-export const WS_URL = "ws://localhost:3000";
-export default BASE_URL;
