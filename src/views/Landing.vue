031ba1b26a41f0f9612972f7c4bc70d306cfde12
Yossig <yosigartner@gmail.com>
2019-06-09 14:52:50 +0300

    test auto deploy

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -13,7 +13,9 @@
       <div class="header__text-box">
         <h1 class="heading-primary">
           <span class="heading-primary--main">Museek</span>
-          <span class="heading-primary--sub">find your stage</span>
+          <span class="heading-primary--sub"
+            >find your stage (Deployed Automatically)</span
+          >
         </h1>
         <router-link :to="'/login'">
           <a class="btn btn--white btn--animated">Sign In</a>
